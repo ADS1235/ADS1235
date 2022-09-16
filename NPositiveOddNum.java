d15417db5307b8d5a3b0eef3e5c81ff1b3a4abc3
AKSHAY DILIP SHINGARE <108734868+ADS1235@users.noreply.github.com>
2022-09-16 18:57:33 +0530

    Add files via upload

---
+++ NPositiveOddNum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NPositiveOddNum {
+	public static void main(String[] args) {
+			int n,i; //i is generally called as counter or loop va
+			System.out.println("Enter the range ");
+			Scanner sc =new Scanner (System.in);
+			n=sc.nextInt();
+			i=1;
+			while(i<=n) {
+			System.out.print((i*2-1)  +    "   ");
+			i++;
+			}
+			}}
+			
+
