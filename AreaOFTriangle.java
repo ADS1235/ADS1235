d5711a2c8953e1dc74fe680daa4166b952a0c050
AKSHAY DILIP SHINGARE <108734868+ADS1235@users.noreply.github.com>
2022-09-16 18:52:17 +0530

    Add files via upload

---
+++ AreaOFTriangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+	public class AreaOFTriangle {
+	public static void main(String[] args) {
+				System.out.println("Enter the base & height");
+		        int  area, b,h;
+				Scanner sc=new Scanner(System.in);
+				b=sc.nextInt();
+				h=sc.nextInt();
+				area=(b*h/2);
+				System.out.println("area of Triangle"  +area);
+				
+			}
+	       }
+
+
