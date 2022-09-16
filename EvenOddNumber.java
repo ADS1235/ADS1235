ba05e8903295e6eb95711eb939e3744f5b890356
AKSHAY DILIP SHINGARE <108734868+ADS1235@users.noreply.github.com>
2022-09-16 18:53:39 +0530

    Add files via upload

---
+++ EvenOddNumber.java
@@ -0,0 +1,18 @@
+
+       public class EvenOddNumber {
+
+       public void EvenOdd(int n) {
+		
+		if(n%2==0)
+		{
+		System.out.println("Even Number");
+		}
+		else
+		System.out.println("Odd Number");
+		}
+	    
+		  public static void main(String[] args) {
+		  EvenOddNumber EON= new EvenOddNumber();
+		    	        EON.EvenOdd(5);
+        }
+	       }
