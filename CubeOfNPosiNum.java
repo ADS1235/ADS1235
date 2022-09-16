01cd6843ce07ae1daf4666d4622e92d0626421a8
AKSHAY DILIP SHINGARE <108734868+ADS1235@users.noreply.github.com>
2022-09-16 18:56:08 +0530

    Add files via upload

---
+++ CubeOfNPosiNum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class CubeOfNPosiNum {
+    
+    public static void main(String[] args) {			
+		int n,i,sum=0; 		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the range : - ");
+		n=sc.nextInt();
+		
+		    i=1;
+		    while(i<=n) {
+			
+			int c=i*i*i;
+			if(i<n)
+			System.out.print(c+" + ");
+			else
+			System.out.print(c);
+			
+			sum=sum+c;
+			i++;
+			
+		  }
+		
+		System.out.println("\n\nSum = "+sum);
+	      }                                                                                 
+       }
+		    
+		  
+	
+
+
+
+
+
