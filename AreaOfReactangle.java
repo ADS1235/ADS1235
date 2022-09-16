1faba399ef4ec0e08a007eb2c156502d7bb69d7a
AKSHAY DILIP SHINGARE <108734868+ADS1235@users.noreply.github.com>
2022-09-16 18:51:50 +0530

    Add files via upload

---
+++ AreaOfReactangle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class AreaOfReactangle {
+	public static void main (String [] args) {
+		 double lenght,breath,area;
+	     Scanner s = new Scanner(System.in);
+	     System.out.print("enter the lenght =");
+	     lenght =s.nextDouble();
+	     System.out.print("enter the   breath  =");
+	     breath =s.nextDouble();
+	     area = lenght*breath;
+	     System.out.println("Area of Reactangle:"+area);
+
+	}}
+
